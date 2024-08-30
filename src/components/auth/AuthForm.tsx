6dd5338d4333bdd556e68ccf902f5d8cd2f4b025
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-08-30 07:26:33 -0500

    chore: increase button size

--- src/components/auth/AuthForm.tsx
+++ src/components/auth/AuthForm.tsx
@@ -1,69 +1,71 @@
 "use client";
-import React from "react";
+import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { Loader2 } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+import { Form } from "@/components/ui/form";
 
-import { LogIn } from "@/app/(auth)/login/page";
 import { useLogin } from "@/hooks/auth/useLogIn";
-import { LogInFormSchema } from "@/lib/validations";
+import { authFormSchema } from "@/lib/validations";
+import FillIcon from "../icons/FillIcons";
+import { signIn } from "next-auth/react";
+import { Separator } from "../ui/separator";
+import CustomInput from "../shared/CustomInput";
 
-const AuthForm = () => {
-  const form = useForm<LogIn>({ resolver: zodResolver(LogInFormSchema) });
-  const { isFailed, isLoading, handleLogIn } = useLogin();
+const AuthForm = ({ type }: { type: string }) => {
+  const formSchema = authFormSchema(type);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+  const { isLoading, handleLogIn } = useLogin();
   return (
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(handleLogIn)}
         className="auth-containers"
       >
-        <FormField
-          control={form.control}
+        <CustomInput
           name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input className="p-6" placeholder="email" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
+          placeholder="email"
+          label="Email"
           control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input
-                  type="password"
-                  className="p-6"
-                  placeholder="enter your password"
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
         />
+        <CustomInput
+          name="password"
+          placeholder="Enter your password"
+          label="Password"
+          control={form.control}
+        />
+
         <Button
-          className="text-md mt-4 p-4 font-bold"
+          className="text-md mt-4 p-6 font-bold"
           disabled={isLoading}
           type="submit"
         >
-          Submit
+          {isLoading ? (
+            <>
+              <Loader2 size={20} className="animate-spin" /> &nbsp; Loading...
+            </>
+          ) : type === "signin" ? (
+            "Sign In"
+          ) : (
+            "Sign Up"
+          )}
+        </Button>
+        <Separator className="my-2" />
+
+        <Button
+          type="button"
+          className="bg-slate-800 p-6 text-lg font-bold hover:bg-slate-500"
+          onClick={() => {
+            signIn("google", { callbackUrl: "/" });
+          }}
+        >
+          <FillIcon.Google className="mr-3 fill-white" />
+          {type === "signin" ? "Sign In with Google" : "Sign Up with Google"}
         </Button>
       </form>
     </Form>
