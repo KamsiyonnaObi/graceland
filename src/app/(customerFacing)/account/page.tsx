f8dd420fae3c034c959a767218e3e06204655383
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-10-16 19:43:19 -0500

    chore: link to products page with zero orders

--- src/app/(customerFacing)/account/page.tsx
+++ src/app/(customerFacing)/account/page.tsx
@@ -32,48 +32,50 @@ const AccountPage = async ({
     <section className="page-container space-y-12">
       <ProfileWelcomeSection />
 
-      <div>
-        <h4 className="mb-2 font-bold">Your recent orders: </h4>
-        <Table className="justify-between rounded-xl border">
-          <TableHeader>
-            <TableRow>
-              <TableHead>Reference</TableHead>
-              <TableHead>Date</TableHead>
-              <TableHead>Status</TableHead>
-              <TableHead>Price Paid</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {orderDetails?.map((order) => (
-              <TableRow key={order.id}>
-                <TableCell>
-                  <Link
-                    className="underline"
-                    href={`checkout/order-success?reference=${order.trxref}`}
-                  >
-                    <div className="flex items-center gap-1">
-                      {order.trxref}
-                      <ArrowUpRight className="h-4 w-4" />
-                    </div>
-                  </Link>
-                </TableCell>
-                <TableCell>
-                  {order.updatedAt.toLocaleString("en-US", {
-                    dateStyle: "medium",
-                  })}
-                </TableCell>
-                <TableCell>
-                  <StatusPill status={order.status} />
-                </TableCell>
-                <TableCell>
-                  {formatCurrency(order.totalPriceInCents / 100)}
-                </TableCell>
+      {orderDetails && orderDetails.length > 0 && (
+        <div>
+          <h4 className="mb-2 font-bold">Your recent orders: </h4>
+          <Table className="justify-between rounded-xl border">
+            <TableHeader>
+              <TableRow>
+                <TableHead>Reference</TableHead>
+                <TableHead>Date</TableHead>
+                <TableHead>Status</TableHead>
+                <TableHead>Price Paid</TableHead>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-        {totalPages && <PaginationComponent totalPages={totalPages} />}
-      </div>
+            </TableHeader>
+            <TableBody>
+              {orderDetails?.map((order) => (
+                <TableRow key={order.id}>
+                  <TableCell>
+                    <Link
+                      className="underline"
+                      href={`checkout/order-success?reference=${order.trxref}`}
+                    >
+                      <div className="flex items-center gap-1">
+                        {order.trxref}
+                        <ArrowUpRight className="h-4 w-4" />
+                      </div>
+                    </Link>
+                  </TableCell>
+                  <TableCell>
+                    {order.updatedAt.toLocaleString("en-US", {
+                      dateStyle: "medium",
+                    })}
+                  </TableCell>
+                  <TableCell>
+                    <StatusPill status={order.status} />
+                  </TableCell>
+                  <TableCell>
+                    {formatCurrency(order.totalPriceInCents / 100)}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+          {totalPages && <PaginationComponent totalPages={totalPages} />}
+        </div>
+      )}
     </section>
   );
 };
--- src/components/account/ProfileWelcomeSection.tsx
+++ src/components/account/ProfileWelcomeSection.tsx
@@ -1,5 +1,6 @@
-import { getUserLatestOrders } from "@/app/admin/_actions/user.actions";
+import Link from "next/link";
 import ProfileOrderCard from "./ProfileOrderCard";
+import { getUserLatestOrders } from "@/app/admin/_actions/user.actions";
 
 const ProfileWelcomeSection = async () => {
   const { userOrders } = await getUserLatestOrders();
@@ -12,20 +13,35 @@ const ProfileWelcomeSection = async () => {
         <p className="text-xl">Manage your order and account details.</p>
       </div>
 
-      <div className="space-y-2">
-        <h4 className="font-bold">Your latest order: </h4>
-        <ProfileOrderCard
-          date={userOrders?.orders[0].updatedAt!}
-          totalPricePaid={userOrders?.orders[0].totalPriceInCents!}
-          productName={userOrders?.orders[0].orderItems[0].product.name!}
-          productImgPath={
-            userOrders?.orders[0].orderItems[0].product.imagePath!
-          }
-          quantity={userOrders?.orders[0].orderItems[0].quantity!}
-          trxref={userOrders?.orders[0].trxref!}
-          totalItems={userOrders?.orders[0].orderItems.length!}
-        />
-      </div>
+      {userOrders?.orders && userOrders.orders.length > 0 ? (
+        <div className="space-y-2">
+          <h4 className="font-bold">Your latest order: </h4>
+          <ProfileOrderCard
+            date={userOrders?.orders[0].updatedAt}
+            totalPricePaid={userOrders?.orders[0].totalPriceInCents}
+            productName={userOrders?.orders[0].orderItems[0].product.name}
+            productImgPath={
+              userOrders?.orders[0].orderItems[0].product.imagePath
+            }
+            quantity={userOrders?.orders[0].orderItems[0].quantity}
+            trxref={userOrders?.orders[0].trxref}
+            totalItems={userOrders?.orders[0].orderItems.length}
+          />
+        </div>
+      ) : (
+        <>
+          <div className="pb-4">
+            Thank you for signing up! Take a look at our latest products and
+            make your first order!
+          </div>
+          <Link
+            className="w-full rounded-md bg-secondary-one p-3 text-center font-bold hover:bg-opacity-75 active:bg-secondary-dark active:text-white"
+            href={"/products"}
+          >
+            Explore our products
+          </Link>
+        </>
+      )}
     </section>
   );
 };
