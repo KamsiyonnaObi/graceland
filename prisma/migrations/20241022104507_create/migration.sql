8ac44cd42067bddd99ac2a638d883ef04f90ca9c
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-10-22 06:25:53 -0500

    chore: update prisma schema to create tokens

---
+++ prisma/migrations/20241022104507_create/migration.sql
@@ -0,0 +1,15 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "verifiedEmail" BOOLEAN NOT NULL DEFAULT false;
+
+-- CreateTable
+CREATE TABLE "Token" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Token_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Token" ADD CONSTRAINT "Token_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
