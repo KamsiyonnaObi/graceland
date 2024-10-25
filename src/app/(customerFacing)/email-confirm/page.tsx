1dafb8129f398dc11a8b5ab174bc526264c8e27d
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-10-25 07:37:49 -0500

    Build email verified page

--- src/app/(customerFacing)/email-confirm/page.tsx
+++ src/app/(customerFacing)/email-confirm/page.tsx
@@ -1,14 +1,29 @@
+import Link from "next/link";
 import React from "react";
 
-const VerifyEmailPage = ({
+import { verifyUserEmail } from "@/utils/actions/user.actions";
+
+const VerifyEmailPage = async ({
   searchParams,
 }: {
   searchParams: { token: string };
 }) => {
+  const { success: isTokenValid } = await verifyUserEmail(searchParams.token);
   return (
-    <div className="page-container">
-      <h1>Hello</h1>
-      <p>Your email is verified, here is your token. {searchParams.token}</p>
+    <div className="py-[72px] lg:py-[152px]">
+      <div className="mx-auto w-fit space-y-4">
+        <h1 className="text-2xl font-bold">
+          {isTokenValid
+            ? "Congratulations! Your email is verified"
+            : "Your token has expired"}
+        </h1>
+        <Link
+          className="mt-4 flex justify-center rounded-2xl bg-secondary-one px-10 py-2 font-bold hover:bg-secondary-two"
+          href={isTokenValid ? "/products" : "/login"}
+        >
+          {isTokenValid ? "View Products" : "Sign In"}
+        </Link>
+      </div>
     </div>
   );
 };
