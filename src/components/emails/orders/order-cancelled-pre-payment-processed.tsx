6ab1bce25b09a4ef0624ab1981c49221b1ef0ab7
Dare Alesh <damilarealesh99@gmail.com>
2025-10-11 05:43:17 -0500

    generic email action function simple implementation

---
+++ src/components/emails/orders/order-cancelled-pre-payment-processed.tsx
@@ -0,0 +1,14 @@
+
+
+export const OrderCancelledPrePaymentProcessed = () => {
+    return (
+      <div>
+        <p>Hi there,</p>
+        <p>
+         Your order has been cancelled.
+        </p>
+       
+        <p>Best,</p> <p>The Graceland Team</p>
+      </div>
+    );
+  };
