aaec3e3f06cdf3dd2b3e869e77cd51a08994a334
Dare Alesh <damilarealesh99@gmail.com>
2025-11-11 23:39:08 -0600

    added info to order shipped email

--- src/components/emails/orders/order-placed.tsx
+++ src/components/emails/orders/order-placed.tsx
@@ -1,12 +1,44 @@
-export const OrderPlaced = () => {
-    return (
-      <div>
-        <p>Hi there,</p>
-        <p>
-        We&apos;ve received your order. Please complete payment to continue.
-        </p>
-       
-        <p>Best,</p> <p>The Graceland Team</p>
-      </div>
-    );
+interface OrderProps {
+  order: {
+    pickUpPersonFirstName: string;
+    pickUpPersonLastName: string;
+    billingLastName: string;
+    billingFirstName: string;
+    phoneNumber: string;
+    createdAt: Number;
+    orderEmail: string;
+    trackingNumber: string;
+    orderNumber: string;
+    trxref: string;
   };
+}
+
+export const OrderPlaced = ({ order }: OrderProps) => {
+  return (
+    <div>
+      <p>Hi there,</p>
+      <p>
+        We&apos;ve received your order. Please complete payment to continue.
+      </p>
+      <p> Order Details </p>
+      <div>
+        {" "}
+        <p>
+          Pickup Person: {order.pickUpPersonFirstName ?? order.billingFirstName}
+          , {order.pickUpPersonLastName ?? order.billingLastName}{" "}
+        </p>{" "}
+      </div>
+      <p> Email: {order.orderEmail}</p>
+      <p> Phone Number: {order.phoneNumber}</p>
+      <p>Pickup Store: 43 Oroyinyin Street, Idumota-Lagos, Nigeria</p>
+      <p> Pickup Documents : A valid id showing first name and last name</p>
+      <a
+        className="underline"
+        href={`/checkout/order-success?reference=${order.trxref}`}
+      >
+        You can view your order summary here
+      </a>
+      <p>Best,</p> <p>The Graceland Team</p>
+    </div>
+  );
+};
